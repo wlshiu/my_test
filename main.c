173641d542bcd763b80ca1d0bdc3688bd2c8aef6
WL Hsu <wlhsu@ambarella.com>
2015-07-08 11:12:52 +0800

    create and ready.

---
+++ main.c
@@ -0,0 +1,129 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "stdint.h"
+#include "stdbool.h"
+#include "ring_buf_opt.h"
+#include "time.h"
+
+typedef struct frame_info
+{
+    uint32_t        order;
+
+    uint32_t        frame_start_offset;
+    uint32_t        frame_size;
+    uint32_t        item_size;
+
+    //uint8_t         *pNext_item_start;
+}frame_info_t;
+
+///////////////////////////////////////////////
+int _get_item_size(uint8_t *w_ptr, uint8_t *r_ptr, uint32_t *pItem_size)
+{
+    frame_info_t    *pFrame_info = (frame_info_t*)r_ptr;
+
+    *pItem_size = pFrame_info->item_size;
+    return 0;
+}
+
+int main()
+{
+    int             i;
+    uint32_t        frame_buf_size = (15 << 10);
+    uint8_t         *pFrame_buf = 0;
+    uint32_t        item_header_size = 0;
+    FILE            *fp_w = 0, *fp_r = 0;
+
+    rb_operator_t   rb_opt = {0};
+
+    srand((unsigned int)time(NULL));
+
+    if( !(pFrame_buf = malloc(frame_buf_size)) )
+    {
+        printf("malloc fail %u\n", __LINE__);
+        return 0;
+    }
+
+    memset(pFrame_buf, 0xa, frame_buf_size);
+
+    rb_opt_init(&rb_opt, pFrame_buf, frame_buf_size);
+
+    item_header_size = sizeof(frame_info_t);
+
+    fp_w = fopen("w_info.txt", "wb");
+    fp_r = fopen("r_info.txt", "wb");
+
+
+    for(i = 1; i < 100000; i++)
+    {
+    	int			rst = 0;
+        uint8_t     *pItem_buf = 0, *pItem_addr = 0;
+        uint32_t    item_size = 0;
+        uint32_t    tmp_buf_size = 0;
+
+        do{
+			tmp_buf_size = (rand() >> 3) & 0xfff;
+        }while( tmp_buf_size == 0 );
+
+        pItem_buf = malloc(tmp_buf_size);
+        memset(pItem_buf, (i & 0xF), tmp_buf_size);
+
+        if( rand() & 0x10 )
+        {
+        	static int		cnt = 0;
+            frame_info_t    frm_info = {0};
+
+            frm_info.order              = cnt++;
+            frm_info.frame_start_offset = item_header_size;
+            frm_info.frame_size         = tmp_buf_size;
+            frm_info.item_size          = tmp_buf_size + item_header_size;
+
+			printf("w: order=%u, item_size=%u, frame_size=%u, data=0x%x \n",
+				frm_info.order,
+				frm_info.item_size,
+				frm_info.frame_size,
+				(i & 0xF));
+
+			rst = rb_opt_update_w(&rb_opt,
+								  (uint8_t*)&frm_info, item_header_size,
+								  pItem_buf, frm_info.frame_size);
+			if( rst == 0 )
+			{
+				fprintf(fp_w, "order=%u, item_size=%u, frame_size=%u, data=0x%x \n",
+					frm_info.order,
+					frm_info.item_size,
+					frm_info.frame_size,
+					(i & 0xF));
+			}
+        }
+        else
+        {
+            frame_info_t    *pFrame_info = 0;
+
+            rb_opt_update_r(&rb_opt, &pItem_addr, &item_size, _get_item_size);
+
+            pFrame_info = (frame_info_t*)pItem_addr;
+            if( pFrame_info )
+            {
+                printf("R: order=%u, item_size=%u, frame_size=%u, data=0x%x \n",
+                    pFrame_info->order,
+                    pFrame_info->item_size,
+                    pFrame_info->frame_size,
+                    *(pItem_addr + pFrame_info->frame_start_offset) & 0xF);
+                fprintf(fp_r, "order=%u, item_size=%u, frame_size=%u, data=0x%x \n",
+                    pFrame_info->order,
+                    pFrame_info->item_size,
+                    pFrame_info->frame_size,
+                    *(pItem_addr + pFrame_info->frame_start_offset) & 0xF);
+            }
+        }
+
+        if( pItem_buf )  free(pItem_buf);
+    }
+
+    fclose(fp_w);
+    fclose(fp_r);
+
+    if( pFrame_buf )    free(pFrame_buf);
+    return 0;
+}
---
+++ ring_buf_opt.h
@@ -0,0 +1,173 @@
+#ifndef __rb_opt_template_H_w2e8RcxR_lasx_HFnD_swSB_u3NtoMPaJ7aa__
+#define __rb_opt_template_H_w2e8RcxR_lasx_HFnD_swSB_u3NtoMPaJ7aa__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdio.h>
+#include <string.h>
+//=============================================================================
+//                Constant Definition
+//=============================================================================
+
+//=============================================================================
+//                Macro Definition
+//=============================================================================
+
+//=============================================================================
+//                Structure Definition
+//=============================================================================
+/**
+ *  ring buffer operator, never stop when write data
+ */
+typedef struct rb_operator
+{
+    unsigned char     *pRead_ptr;
+    unsigned char     *pWrite_ptr;
+    unsigned char     *pBuf_start_ptr;
+    unsigned char     *pBuf_end_ptr;
+    unsigned char     *pValid_end_ptr;
+}rb_operator_t;
+
+typedef int (*get_item_size)(unsigned char *w_ptr, unsigned char *r_ptr, unsigned int *pItem_size);
+//=============================================================================
+//                Global Data Definition
+//=============================================================================
+
+//=============================================================================
+//                Private Function Definition
+//=============================================================================
+static int
+rb_opt_init(
+    rb_operator_t   *pRbOpt,
+    unsigned char   *pStart_ptr,
+    unsigned int    buf_size)
+{
+    if( !pRbOpt )   return -1;
+    pRbOpt->pBuf_start_ptr = pStart_ptr + (buf_size & 0x3);
+    pRbOpt->pRead_ptr      = pRbOpt->pBuf_start_ptr;
+    pRbOpt->pWrite_ptr     = pRbOpt->pBuf_start_ptr;
+    pRbOpt->pBuf_end_ptr   = pStart_ptr + buf_size;
+    pRbOpt->pValid_end_ptr = pRbOpt->pBuf_end_ptr;
+    return 0;
+}
+
+static int
+rb_opt_update_w(
+    rb_operator_t   *pRbOpt,
+    unsigned char   *pHeader,
+    unsigned int    header_size,
+    unsigned char   *pData,
+    unsigned int    data_size)
+{
+    unsigned char    *w_ptr = pRbOpt->pWrite_ptr;
+    unsigned char    *r_ptr = pRbOpt->pRead_ptr;
+    unsigned int     item_size = header_size + data_size;
+
+    // 32 bits alignment
+    item_size = (item_size + 0x3) & ~0x3;
+
+    if( w_ptr > r_ptr )
+    {
+        if( (w_ptr + item_size) > pRbOpt->pBuf_end_ptr )
+        {
+            if( (pRbOpt->pBuf_start_ptr + item_size) >= r_ptr )
+            {
+                printf("\t!! no space to write, drop current item !!\n");
+                return -1;
+            }
+
+            pRbOpt->pValid_end_ptr = (unsigned char*)(((unsigned int)w_ptr + 0x3) & ~0x3);
+            printf("\t!! w-> valid_end 0x%x\n", w_ptr);
+            w_ptr = pRbOpt->pBuf_start_ptr;
+        }
+    }
+    else
+    {
+        if( w_ptr != r_ptr &&
+            (w_ptr + item_size) >= r_ptr )
+        {
+            printf("\t!! write catch read, drop current item !!\n");
+            return -1;
+        }
+    }
+
+
+    printf("\t write_ptr = 0x%x\n", w_ptr);
+
+    memcpy((void*)w_ptr, (void*)pHeader, header_size);
+    w_ptr += header_size;
+    memcpy((void*)w_ptr, (void*)pData, data_size);
+    w_ptr += data_size;
+
+    // 32 bits alignment
+    w_ptr = (unsigned char*)(((unsigned int)w_ptr + 0x3) & ~0x3);
+
+    pRbOpt->pWrite_ptr = w_ptr;
+    return 0;
+}
+
+static int
+rb_opt_update_r(
+    rb_operator_t   *pRbOpt,
+    unsigned char   **ppData,
+    unsigned int    *pData_size,
+    get_item_size   cb_get_item_size)
+{
+    unsigned char     *w_ptr = pRbOpt->pWrite_ptr;
+    unsigned char     *r_ptr = pRbOpt->pRead_ptr;
+    unsigned int      item_size = 0;
+    unsigned int      item_size_align = 0;
+
+    if( cb_get_item_size == NULL )
+        return  -1;
+
+    cb_get_item_size(w_ptr, r_ptr, &item_size);
+    if( item_size == 0 )
+    {
+        *ppData     = NULL;
+        *pData_size = 0;
+        return -2;
+    }
+
+    // 32 bits alignment
+    item_size_align = (item_size + 0x3) & ~0x3;
+
+    if( w_ptr < r_ptr ||
+        (r_ptr + item_size_align) < w_ptr )
+    {
+        *ppData     = r_ptr;
+        *pData_size = item_size;
+
+        printf("\t read_ptr  = 0x%x\n", r_ptr);
+
+        r_ptr += item_size_align;
+
+        if( r_ptr == pRbOpt->pValid_end_ptr )
+        {
+            pRbOpt->pValid_end_ptr = pRbOpt->pBuf_end_ptr;
+            r_ptr = pRbOpt->pBuf_start_ptr;
+
+            printf("!! r_ptr at valid_end 0x%x\n", r_ptr);
+        }
+    }
+    else
+    {
+        *ppData     = NULL;
+        *pData_size = 0;
+    }
+
+    pRbOpt->pRead_ptr = r_ptr;
+
+    return 0;
+}
+//=============================================================================
+//                Public Function Definition
+//=============================================================================
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
