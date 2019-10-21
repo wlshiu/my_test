40398bde74df38369d830b0a284686ae25a46a98
Wei-Lun <wl@mail.com>
2019-10-21 21:20:56 +0800

    add CmBacktrace-1.3.0

--- main.c
+++ main.c
@@ -11,8 +11,8 @@
  */
 
 
-#include "backtrace.h"
 
+#include "cm_backtrace.h"
 //=============================================================================
 //                  Constant Definition
 //=============================================================================
@@ -32,11 +32,58 @@
 //=============================================================================
 //                  Private Function Definition
 //=============================================================================
+static void fault_test_by_unalign(void)
+{
+    volatile int * SCB_CCR = (volatile int *) 0xE000ED14; // SCB->CCR
+    volatile int * p;
+    volatile int value;
+
+    *SCB_CCR |= (1 << 3); /* bit3: UNALIGN_TRP. */
+
+    p = (int *) 0x00;
+    value = *p;
+    printf("addr:0x%02X value:0x%08X\r\n", (int) p, value);
+
+    p = (int *) 0x04;
+    value = *p;
+    printf("addr:0x%02X value:0x%08X\r\n", (int) p, value);
+
+    p = (int *) 0x03;
+    value = *p;
+    printf("addr:0x%02X value:0x%08X\r\n", (int) p, value);
+}
+
+static void fault_test_by_div0(void)
+{
+    volatile int * SCB_CCR = (volatile int *) 0xE000ED14; // SCB->CCR
+    int x, y, z;
+
+    *SCB_CCR |= (1 << 4); /* bit4: DIV_0_TRP. */
+
+    x = 10;
+    y = 0;
+    z = x / y;
+    printf("z:%d\n", z);
+}
 
 //=============================================================================
 //                  Public Function Definition
 //=============================================================================
+void HardFault_Handler(void)
+{
+#ifdef __ARM__
+    __asm("MOV     r0, lr               \n" /* get lr */
+          "MOV     r1, sp               \n" /* get stack pointer (current is MSP) */
+          "BL      cm_backtrace_fault   \n");
+
+    __asm volatile("BKPT #01");
+#endif
+}
+
 int main(void)
 {
+    cm_backtrace_init("CmBacktrace", "v0.0.0", "v1.3.0");
+
+    fault_test_by_div0();
     return 0;
 }
