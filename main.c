653fb552351bf0631686a9a9efb3cad16de626bd
Wei-Lun <wl@mail.com>
2019-11-07 22:21:04 +0800

    init

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+extern int ex_buffer(void);
+extern int ex_codelock(void);
+extern int ex_small(void);
+
+int main()
+{
+    ex_buffer();
+//    ex_codelock();
+//    ex_small();
+
+    return 0;
+}
