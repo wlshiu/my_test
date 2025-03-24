43fb8d0dafdacc67561aa574624b0d53a41a917d
Wei-Lun <wl_hsu@padauk.com.tw>
2025-03-24 17:42:22 +0800

    backup

--- main.c
+++ main.c
@@ -92,6 +92,53 @@ static inline float range_to_0_360(float degree)
 //=============================================================================
 //                  Private Function Definition
 //=============================================================================
+
+#define CORDIC_MAXITER  16
+#define CORDIC_PI       0x10000000
+
+int CORDIC_ZTBL[] = {
+    0x04000000, 0x025C80A4, 0x013F670B, 0x00A2223B,
+    0x005161A8, 0x0028BAFC, 0x00145EC4, 0x000A2F8B, 0x000517CA, 0x00028BE6,
+    0x000145F3, 0x0000A2FA, 0x0000517D, 0x000028BE, 0x0000145F, 0x00000A30
+};
+
+int fxpt_atan2(int y, int x)
+{
+	int k, tx, z = 0, fl = 0;
+
+	if (x < 0)
+    {
+		fl = ((y > 0) ? 1 : -1);
+		x = -x;
+		y = -y;
+	}
+
+	for (k = 0; k < CORDIC_MAXITER; k++)
+    {
+		tx = x;
+		if (y <= 0)
+        {
+			x -= (y >> k);
+			y += (tx >> k);
+			z -= CORDIC_ZTBL[k];
+		}
+		else
+        {
+			x += (y >> k);
+			y -= (tx >> k);
+			z += CORDIC_ZTBL[k];
+		}
+	}
+
+	if (fl != 0)
+    {
+		z += fl * CORDIC_PI;
+	}
+
+	return z;   // radians multiplied by factor M
+	//*m = x; // sqrt(x^2+y^2) multiplied by gain F
+}
+
 static void
 _test_arm_sin_cos(void)
 {
@@ -160,16 +207,21 @@ _test_arm_atan2(void)
         ideal_cos    = cos(degree2rad(degree));
         ideal_radian = degree2rad(degree);
 
-    #if 1
+    #if 0
         /* Q31 atan2 */
         q31_t   sim_radian = 0;
         ideal_radian = Q29(ideal_radian);
         arm_atan2_q31((q31_t)Q24(ideal_sin), (q31_t)Q24(ideal_cos), &sim_radian);
-    #elif 1
+    #elif 0
         /* Q15 atan2 */
         q15_t   sim_radian = 0;
         ideal_radian = Q13(ideal_radian);
         arm_atan2_q15((q15_t)Q15(ideal_sin), (q15_t)Q15(ideal_cos), &sim_radian);
+    #elif 1
+        int16_t     sim_radian = 0;
+
+        ideal_radian = Q26(ideal_radian);
+        sim_radian = fxpt_atan2(Q26(ideal_sin), Q26(ideal_cos));// >> 15;
     #else
         /* float */
         float32_t   sim_radian = 0.0f;
@@ -254,10 +306,37 @@ _test_arm_cordic_rotation(void)
 //=============================================================================
 int main()
 {
-    _test_arm_sin_cos();
+#if 1
+	for (int i = 0; i < 360; i += 1)
+    {
+		int16_t     cos_val = 32767.0f * cos((float)i * PI / 180.0f);
+		int16_t     sin_val = 32767.0f * sin((float)i * PI / 180.0f);
+		uint32_t    atan2_clocks = 0;
+		uint32_t    atan2f_clocks = 0;
+		uint32_t    fxpt_at2_clocks = 0;
 
-//    _test_arm_atan2();
+		int16_t at2f = 32767.0f * (float)atan2f((float)sin_val, (float)cos_val) / PI;
+
+
+		int16_t fxpt_at2 = fxpt_atan2(sin_val << 15, cos_val << 15) >> 13;
+
+
+		//float dif = 100.0f*abs(at2-fxpt_at2)/at2;
+
+		int16_t iang = 2 * 32767.0f * i / 360.0f;
+
+		printf(
+				" angle = %i(%i), fxpt_atan2 = %i, atan2f = %i, sn= %i cs=%i \n",
+				i, iang, fxpt_at2, at2f, sin_val, cos_val);
+
+	}
+#else
+
+//    _test_arm_sin_cos();
+
+    _test_arm_atan2();
 
 //    _test_arm_cordic_rotation();
+#endif // 0
     return 0;
 }
